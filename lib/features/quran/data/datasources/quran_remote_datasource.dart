d6097071973f6e2fcef7d80e448743a90f519f9f
idemir <isademir.dev@gmail.com>
2024-12-07 21:56:26 +0300

    verse datasource addded

--- lib/features/quran/data/datasources/quran_remote_datasource.dart
+++ lib/features/quran/data/datasources/quran_remote_datasource.dart
@@ -1,14 +1,14 @@
-import 'dart:convert';
-
 import 'package:daily_message/core/api/api_endpoints.dart';
 import 'package:daily_message/core/api/base_api_service.dart';
 import 'package:daily_message/core/exceptions/exception.dart';
 import 'package:daily_message/features/quran/data/models/author_model.dart';
 import 'package:daily_message/features/quran/data/models/surah_model.dart';
+import 'package:daily_message/features/quran/data/models/verse_data_model.dart';
 
 abstract class IQuranDatasource {
   Future<AuthorListModel> getAuthors();
   Future<SurahListModel> getSurahs();
+  Future<VerseDataModel> getVerse(int surahId, int verseNo, int authorId);
 }
 
 class QuranRemoteDataSource implements IQuranDatasource {
@@ -37,6 +37,21 @@ class QuranRemoteDataSource implements IQuranDatasource {
       final surahListModel = SurahListModel.fromJson(resp.data);
 
       return surahListModel;
+    } catch (e) {
+      throw ServerException(message: 'Veriler alinamadi!');
+    }
+  }
+
+  @override
+  Future<VerseDataModel> getVerse(
+      int surahId, int verseNo, int authorId) async {
+    try {
+      final resp = await _apiService.getRequest(
+          endPoint: '/surah/$surahId/verse/$verseNo?author=$authorId');
+
+      final verseDataModel = VerseDataModel.fromJson(resp.data);
+
+      return verseDataModel;
     } catch (e) {
       throw ServerException(message: 'Veriler alinamadi!');
     }
